8a5dc56d869df3bd3063110d1548a2ac64886ddf
Nathan Tannar <nathantannar4@gmail.com>
2018-12-19 13:46:11 -0800

    Update Sevrer

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server "nathantannar.me", user: "nathantannar", roles: %w{app db web}, my_property: :my_value
+server "instance-1.us-west1-a.nathan-tannar", user: "nathantannar", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
